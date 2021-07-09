698cac11c98feddf2a23740ede18a1a4a04ac1bf
Tirtha Sarker <tsark99@gmail.com>
2021-07-09 18:43:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Daily Mantra
+
+Know quotes & trivia for your personal growth.
+
+## Run the following commands:
+```
+npm install
+npm run dev
+
+
+Go to http://localhost:3000
+```
+
---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+
+// Set static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.options('*', cors())
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
